0cd5663bc52e0e9e05ab565e2f77ff4998c77a0a
ak97-hub <79121728+ak97-hub@users.noreply.github.com>
2021-10-20 07:42:56 -0500

    Update spark.py

--- spark.py
+++ spark.py
@@ -5,8 +5,8 @@ from pyspark import SparkConf, SparkContext
 import collections
 import os
 
-jdk.install('8', jre=True)
-#jdk.install(8)
+jdk.install('8', jre=True, path="/usr/local/lib/jvm/java-8-openjdk-amd64")
+
 os.environ["JAVA_HOME"] = "/home/appuser/venv/lib/python3.7/site-packages/jdk"
 header = st.container()
 #Set master node on a local machine not in cluster
