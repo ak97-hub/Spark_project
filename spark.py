c522ab155ef86c01c2165f53eff77a94b632885f
ak97-hub <79121728+ak97-hub@users.noreply.github.com>
2021-10-20 06:52:28 -0500

    Add files via upload

---
+++ spark.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from pyspark import SparkConf, SparkContext
+import collections
+
+header = st.container()
+#Set master node on a local machine not in cluster
+#setappname appears in spark UI
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+
+#CREATE sparkContext object
+sc = SparkContext(conf = conf)
+
+#load data into SC
+lines = sc.textFile("/Users/angelokhan/Documents/Projects/Spark_project/u.data")
+
+#extract data we care about
+#spliting each into individual fields by white space
+#the uses[2] to take out the rating value
+ratings = lines.map(lambda x: x.split()[2])
+
+#count unique values
+result = ratings.countByValue()
+
+#sorts results of dictionary
+sortedResults = collections.OrderedDict(sorted(result.items()))
+
+with header:
+#prints results
+    for key, value in sortedResults.items():
+        st.text("{}, {}".format(key, value))
+        print("%s %i" % (key, value))
+
+sc.stop()
